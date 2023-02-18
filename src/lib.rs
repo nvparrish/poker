ff1e9cf12b298e12280f62da1b304606a435d069
Nathan Parrish <nvparrish@gatech.edu>
2023-02-18 14:29:39 -0500

    Added rank and start of comparison
    This commit adds a RankOfValue to turn &str to an integer for comparing
    values.  char was changed to &str because 10 is two characters.
    Currently &str is being used everywhere, so lifetimes were also added.
    Initial comparison of 5 of a kind was created for FiveOfAKind

--- src/lib.rs
+++ src/lib.rs
@@ -4,27 +4,29 @@
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 use std::cmp::Ordering;
 
+const CARD_VALUES: [&'static str; 13] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
     // unimplemented!("Out of {hands:?}, which hand wins?")
     hands.to_vec()
 }
 
-enum PokerHands{
-    FiveOfAKind{value: char},
-    StraightFlush{high_value: char, suit: char},
-    FourOfAKind{value: char, other_card:char},
-    FullHouse{triplet_value: char, pair_value: char},
-    Flush{values: Vec<char>},
-    Stright{high_value: char},
-    ThreeOfAKind{value: char, other_cards: Vec<char>},
-    TwoPair{value1: char, value2: char, other_card: char},
-    OnePair{value: char, other: Vec<char>},
-    HighCard{value: Vec<char>}
+enum PokerHands<'a>{
+    FiveOfAKind{value: &'a str},
+    StraightFlush{high_value: &'a str},
+    FourOfAKind{value: &'a str, other_card:&'a str},
+    FullHouse{triplet_value: &'a str, pair_value: &'a str},
+    Flush{values: Vec<&'a str>},
+    Stright{high_value: &'a str},
+    ThreeOfAKind{value: &'a str, other_cards: Vec<&'a str>},
+    TwoPair{value1: &'a str, value2: &'a str, other_card: &'a str},
+    OnePair{value: &'a str, other: Vec<&'a str>},
+    HighCard{value: Vec<&'a str>}
 }
 
 struct Hand<'a> {
     hand: &'a str,
-    evaluation: PokerHands,
+    evaluation: PokerHands<'a>,
 }
 
 impl<'a> PartialOrd for Hand<'a> {
@@ -33,8 +35,27 @@ impl<'a> PartialOrd for Hand<'a> {
     }
 }
 
+
+fn RankOfValue(value: &str) -> Option<usize> {
+    CARD_VALUES.iter().position(|&r| r == value)
+}
+
 impl<'a> PartialEq for Hand<'a> {
     fn eq(&self, other: &Self) -> bool {
-        false
+        match self.evaluation {
+            PokerHands::FiveOfAKind{value: value1} => {
+                match other.evaluation {
+                    PokerHands::FiveOfAKind{value: value2} => {
+                        if Some(RankOfValue(value1)) == Some(RankOfValue(value2)) {
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                    _ => false
+                }
+            }
+            _ => false
+        }
     }
 }
