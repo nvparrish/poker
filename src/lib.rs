40a3b8d7bf3657a89ee90284065d389abe891d57
Nathan Parrish <nvparrish@gatech.edu>
2023-02-20 17:29:36 -0500

    STart of code to evaluate hands
    This has code to pull out the values and the suits for each card in the
    deck as the first step to being able to compare hands.

--- src/lib.rs
+++ src/lib.rs
@@ -6,38 +6,56 @@ use std::cmp::Ordering;
 
 const CARD_VALUES: [&'static str; 13] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
+fn rank_of_value<'a>(value: &'a str) -> Option<usize> {
+    CARD_VALUES.iter().position(|&r| r == value)
+}
+
+fn compare_rank_of_values<'a, 'b>(a: &'a Option<usize>, b: &'b Option<usize>) -> Ordering {
+    a.cmp(b)
+}
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
     // unimplemented!("Out of {hands:?}, which hand wins?")
     hands.to_vec()
 }
 
-enum PokerHands<'a>{
-    FiveOfAKind{value: &'a str},
-    StraightFlush{high_value: &'a str},
-    FourOfAKind{value: &'a str, other_card:&'a str},
-    FullHouse{triplet_value: &'a str, pair_value: &'a str},
-    Flush{values: Vec<&'a str>},
-    Stright{high_value: &'a str},
-    ThreeOfAKind{value: &'a str, other_cards: Vec<&'a str>},
-    TwoPair{value1: &'a str, value2: &'a str, other_card: &'a str},
-    OnePair{value: &'a str, other: Vec<&'a str>},
-    HighCard{value: Vec<&'a str>}
+enum PokerHands{
+    FiveOfAKind{value: Option<usize>},
+    StraightFlush{high_value: Option<usize>},
+    FourOfAKind{value: Option<usize>, other_card: Option<usize>},
+    FullHouse{triplet_value: Option<usize>, pair_value: Option<usize>},
+    Flush{values: Vec<Option<usize>>},
+    Stright{high_value: Option<usize>},
+    ThreeOfAKind{value: Option<usize>, other_cards: Vec<Option<usize>>},
+    TwoPair{value1: Option<usize>, value2: Option<usize>, other_card: Option<usize>},
+    OnePair{value: Option<usize>, other: Vec<Option<usize>>},
+    HighCard{value: Vec<Option<usize>>}
 }
 
 struct Hand<'a> {
     hand: &'a str,
-    evaluation: PokerHands<'a>,
+    evaluation: PokerHands,
 }
 
+fn EvaluateHand<'a>(hand: &'a str) -> PokerHands {
+    let split = hand.split(' ').collect::<Vec<_>>();
+    let mut values: Vec<Option<usize>> = Vec::with_capacity(split.capacity());
+    let mut suits: Vec<char> = Vec::with_capacity(split.capacity());
+    for s in &split {
+        // let mut s = String::new();
+        // v[0..v.len()-1].clone_into(&mut s);
+        values.push(rank_of_value(&s[0..s.len()-1]));
+        values.sort_unstable_by(compare_rank_of_values);
+        suits.push(s.chars().last().unwrap());
+    }
+    // Check for a flush
+    let flush = suits.iter().all(|&item| item == suits[0]);
+
+    PokerHands::HighCard{value: values}
+}
 impl<'a> PartialOrd for Hand<'a> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(Ordering::Equal)
     }
-}
-
-
-fn RankOfValue(value: &str) -> Option<usize> {
-    CARD_VALUES.iter().position(|&r| r == value)
 }
 
 impl<'a> PartialEq for Hand<'a> {
@@ -46,7 +64,7 @@ impl<'a> PartialEq for Hand<'a> {
             PokerHands::FiveOfAKind{value: value1} => {
                 match other.evaluation {
                     PokerHands::FiveOfAKind{value: value2} => {
-                        if Some(RankOfValue(value1)) == Some(RankOfValue(value2)) {
+                        if Some(&value1) == Some(&value2) {
                             true
                         } else {
                             false
